48f03e567040bb1d97e2725c1105140a18445326
Kuera <makai@uanistudio.com>
2026-07-29 17:27:13 +0800

    fix: expose static HDR lifecycle type to playground build

---
+++ apps/playground/src/static-hdr-mean-augmentation.d.ts
@@ -0,0 +1,7 @@
+import '../../../packages/viewer/src/effects/StaticHdrMeanNode';
+
+declare module '../../../packages/viewer/src/effects/StaticHdrMeanNode' {
+  interface StaticHdrMeanNode {
+    updateBeforeType: string;
+  }
+}
