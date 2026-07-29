9c6d6d8eabacf66e1910f36d3df4d96ad6aab568
Kuera <makai@uanistudio.com>
2026-07-29 17:31:18 +0800

    chore: remove playground type augmentation

--- apps/playground/src/static-hdr-mean-augmentation.d.ts
+++
@@ -1,7 +0,0 @@
-import '../../../packages/viewer/src/effects/StaticHdrMeanNode';
-
-declare module '../../../packages/viewer/src/effects/StaticHdrMeanNode' {
-  interface StaticHdrMeanNode {
-    updateBeforeType: string;
-  }
-}
