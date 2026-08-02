9324e7b7fb25f47b08249e61879ac7146a4f417f
Kuera <makai@uanistudio.com>
2026-08-02 23:21:52 +0800

    chore: declare glTF authoring capability gates

---
+++ apps/studio/src/gltf-authoring-capabilities.ts
@@ -0,0 +1,12 @@
+export const GLTF_AUTHORING_CAPABILITIES = Object.freeze({
+  skinJoints: true,
+  morphTargets: true,
+  draco: true,
+  meshopt: true,
+  ktx2Basis: true,
+  externalResources: true,
+  cameras: true,
+  punctualLights: true,
+  materialVariants: true,
+  overridePreservingReimport: true,
+});
