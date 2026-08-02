c3d59a0f9e70754e592560b97228579ee51935de
Kuera <makai@uanistudio.com>
2026-08-02 13:09:59 +0800

    feat(viewport): install Studio viewport command bridge

--- packages/viewer-adapter/src/public.ts
+++ packages/viewer-adapter/src/public.ts
@@ -1,5 +1,7 @@
 export * from './index';
 import './boxSelection';
+import { installEditorViewportCommandBridge } from './editorViewportCommandBridge';
 import { installNativeTransformBridge } from './nativeTransformBridge';
 
 installNativeTransformBridge();
+installEditorViewportCommandBridge();
