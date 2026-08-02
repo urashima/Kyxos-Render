2eaba665e4a0d68c039dc69f18e44a612de48be6
Kuera <makai@uanistudio.com>
2026-08-02 13:46:04 +0800

    feat: publish transform pivot adapter API

--- packages/viewer-adapter/src/public.ts
+++ packages/viewer-adapter/src/public.ts
@@ -1,6 +1,8 @@
 export * from './index';
 export type { EditorViewportCommand } from './editorViewportCommandBridge';
+export type { TransformPivot } from './transformPivot';
 import './boxSelection';
+import './transformPivot';
 import { installEditorViewportCommandBridge } from './editorViewportCommandBridge';
 import { installNativeTransformBridge } from './nativeTransformBridge';
 
