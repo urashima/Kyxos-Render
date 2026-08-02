73872f868bbba19275651bd86459f23bb6cccbce
Kuera <makai@uanistudio.com>
2026-08-02 13:58:33 +0800

    feat: publish viewport helper adapter API

--- packages/viewer-adapter/src/public.ts
+++ packages/viewer-adapter/src/public.ts
@@ -1,8 +1,10 @@
 export * from './index';
 export type { EditorViewportCommand } from './editorViewportCommandBridge';
 export type { TransformPivot } from './transformPivot';
+export type { ViewportHelperSettings } from './viewportHelpers';
 import './boxSelection';
 import './transformPivot';
+import './viewportHelpers';
 import { installEditorViewportCommandBridge } from './editorViewportCommandBridge';
 import { installNativeTransformBridge } from './nativeTransformBridge';
 
