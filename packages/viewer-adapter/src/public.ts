6c892fcaef59f2d2ccc2fc6f1bcd13e06b0a853d
Kuera <makai@uanistudio.com>
2026-08-03 10:55:15 +0800

    feat(viewer-adapter): install backend acceptance override

--- packages/viewer-adapter/src/public.ts
+++ packages/viewer-adapter/src/public.ts
@@ -12,10 +12,12 @@ export type { ViewportHelperSettings } from './viewportHelpers';
 import './boxSelection';
 import './transformPivot';
 import './viewportHelpers';
+import { installBackendQueryOverride } from './backendQueryOverride';
 import { installEditorViewportCommandBridge } from './editorViewportCommandBridge';
 import { installNativeTransformBridge } from './nativeTransformBridge';
 import { installNonBlockingThumbnailCapture } from './thumbnailCapture';
 
+installBackendQueryOverride();
 installNativeTransformBridge();
 installNonBlockingThumbnailCapture();
 installEditorViewportCommandBridge();
