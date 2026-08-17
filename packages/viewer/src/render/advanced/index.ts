ba86c512f143bfedb78f0b49e7c6e7642afcccc9
Kuera <makai@uanistudio.com>
2026-08-17 15:56:10 +0800

    Export advanced render feature graph

--- packages/viewer/src/render/advanced/index.ts
+++ packages/viewer/src/render/advanced/index.ts
@@ -1,5 +1,6 @@
 export * from './backendCapabilities';
 export * from './bvh';
+export * from './featureGraph';
 export * from './radianceCache';
 export * from './sampling';
 export * from './temporalHistory';
